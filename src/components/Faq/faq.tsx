4e39d5343d5c7c360c5d270c3f5b337da297533f
Meirambekov Nursultan <meirambekov.n2005@gmail.com>
2024-01-28 11:54:39 +0600

    fifth

--- src/components/Faq/faq.tsx
+++ src/components/Faq/faq.tsx
@@ -1,32 +1,31 @@
 import mascout from '../../assets/imgs/mascout.svg';
-import logo from '../../assets/imgs/logo.svg';
 import { useState } from 'react';
 import axios from 'axios';
+import eagle from '../../assets/imgs/eagle.svg';
+import arrow from '../../assets/icons/arrow.svg';
+import micro from '../../assets/icons/micro.svg';
 export default function Footer() {
     const [prompt, setPrompt] = useState("");
     const [response, setResponse] = useState("");
 
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        axios.post("http://127.0.0.1:5173", {prompt}).then((res)=>{
-            setResponse(res.data);
-        }).catch((err)=>{
-            console.error(err);
-        })
-    }
+    // const handleSubmit = (e) =>{
+    //     e.preventDefault();
+    //     axios.post("http://127.0.0.1:5173", {prompt}).then((res)=>{
+    //         setResponse(res.data);
+    //     }).catch((err)=>{
+    //         console.error(err);
+    //     })
+    // }
 
     return(
         <>
             <section className="text-gray-600 overflow-hidden">
-                <div>
-                    <button onClick={handleSubmit}>send</button>
+                {/* <div>
+                    <button>send</button>
                     <input type="text" value={prompt} onChange={(e)=>setPrompt(e.target.value)}/>
                     <p>{response}</p>
-                </div>
-                <div className='mx-auto my-6 flex flex-col gap-3 w-full h-24'>
-                    <img className="lg:w-1/2 w-48 h-16 m-auto" src={logo}/> 
-                    <h2 className='font-poppins font-bold tracking-wide text-center text-[#323232] text-4xl'>Задай вопрос нашему <span className='text-[#30A879]'>ассистенту!</span></h2>
-                </div>
+                </div> */}
+{/*                
                 <div className="container px-5 py-24 mx-auto">
                     <div className="lg:w-4/5 mx-auto flex flex-wrap">
                         <img className="lg:w-1/2 w-full lg:h-auto " src={mascout}/> 
@@ -35,6 +34,20 @@ export default function Footer() {
                             <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">The Catcher in the Rye</h1>
                             <p className="leading-relaxed">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.</p>
                         </div>
+                    </div>
+                </div> */}
+                <div className="container px-5 py-24 mx-auto w-[1240px] relative flex flex-col gap-8">
+                    <div className='mx-auto my-6 w-full h-24'>
+                        <h2 className='font-poppins font-bold tracking-wide text-center text-[#323232] text-4xl'>Задай вопрос нашему <span className='text-[#30A879]'>ассистенту!</span></h2>
+                    </div>
+                    <div  className='absolute left-[370px] top-[350px] w-[850px] h-[240px] border border-[1px] border-black shadow-xl rounded-2xl'>
+                        <p className='p-6 text-[18px]'>Для краткосрочных накоплений рекомендуется использовать банковские депозиты, а для долгосрочных целей, таких как образование детей или пенсия, подходит накопительное страхование жизни. Страхование жизни обеспечивает не только накопление средств, но и страховку на жизнь. В случае смерти страхователя, выгодоприобретатели получают полную страховую сумму. Страхование также может включать защиту от инвалидности, при этом выплачивается полная сумма по окончании срока. </p>
+                    </div>
+                    <div className=''>
+                       <div>
+                       <img src={eagle} />
+                        <input type="email" id="helper-text" aria-describedby="helper-text-explanation" className="w-full bg-gray-50 border border-black-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5" placeholder="Напишите ваш вопрос" ></input>
+                       </div>
                     </div>
                 </div>
             </section>
--- src/components/Hero/hero.tsx
+++ src/components/Hero/hero.tsx
@@ -4,13 +4,14 @@
 import { Carousel } from 'flowbite-react';
 import hero from '../../assets/imgs/hero.svg';
 import eagle from '../../assets/imgs/eagle.svg';
+import halykk from '../../assets/imgs/halykk.svg';
 
 export default function Hero() {
   return (
     <div className='font-poppins py-4 px-4 flex flex-col gap-8'>
         <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 bg-[#FFDD87] rounded-2xl flex flex-row justify-center items-center w-[1240px] mx-auto">
             <div>
-                <img src={eagle} />
+                <img src={eagle}/>
             </div>
             <div className='w-[690px]'>
                 <h1 className='font-bold text-6xl p-6'>
@@ -23,75 +24,73 @@ export default function Hero() {
             <div className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] w-80 h-[420px]">
                 <a href="#!">
                     <img
-                    className="rounded-t-lg"
-                    src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg"
+                    className="rounded-t-lg w-full h-auto"
+                    src={halykk}
                     alt="" />
                 </a>
                 <div className="p-6">
                     <h5
                     className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-                    Card title
+                    Halyk-Life проводит #InsurTechHackathon 
                     </h5>
                     <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
+                    АО «Халык-Life» проведет хакатон InsureTechHackaton для студентов МУИТ и других университетов
                     </p>
                     <button
                     type="button"
                     className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
                     data-te-ripple-init
                     data-te-ripple-color="light">
-                    Button
+                    Узнать 
                     </button>
                 </div>
             </div>
             <div className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] w-80 h-[420px]">
                 <a href="#!">
                     <img
-                    className="rounded-t-lg"
-                    src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg"
+                    className="rounded-t-lg w-full h-auto"
+                    src={halykk}
                     alt="" />
                 </a>
                 <div className="p-6">
                     <h5
                     className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-                    Card title
+                    Объявление о проведение открытого тендера
+
                     </h5>
                     <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
+                    АО «Халык-Life» проведет хакатон InsureTechHackaton для студентов МУИТ и других университетов
                     </p>
                     <button
                     type="button"
                     className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
                     data-te-ripple-init
                     data-te-ripple-color="light">
-                    Button
+                    Узнать
                     </button>
                 </div>
             </div>
             <div className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] w-80 h-[420px]">
                 <a href="#!">
                     <img
-                    className="rounded-t-lg"
-                    src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg"
+                    className="rounded-t-lg w-full h-auto"
+                    src={halykk}
                     alt="" />
                 </a>
                 <div className="p-6">
                     <h5
                     className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-                    Card title
+                    Halyk-Life проводит #InsurTechHackathon 
                     </h5>
                     <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
+                    АО «Халык-Life» проведет хакатон InsureTechHackaton для студентов МУИТ и других университетов
                     </p>
                     <button
                     type="button"
                     className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
                     data-te-ripple-init
                     data-te-ripple-color="light">
-                    Button
+                    Узнать
                     </button>
                 </div>
             </div>
